558bdc53fad79e829118bca0765f2f49539b7431
unknown <gabrielrozas1996@gmail.com>
2023-04-19 13:59:50 -0800

    Fix linter

--- features.js
+++ features.js
@@ -7,7 +7,7 @@ const menubutton = document.querySelector('.menu');
 
 const clickHandler = () => {
   // eslint-disable-next-line no-restricted-globals
-  if (event.target !== menumobile && !event.target.classList.contains('spanline1') && !event.target.classList.contains('menu') && !menumobile.classList.contains('dnone')) { 
+  if (event.target !== menumobile && !event.target.classList.contains('spanline1') && !event.target.classList.contains('menu') && !menumobile.classList.contains('dnone')) {
     menumobile.classList.toggle('dnone');
     document.removeEventListener('click', clickHandler);
   }
